23af14aea3aed7d2506f02acc97e124748fb4694
Raphael Vacari <raphaelvacari7@gmail.com>
2023-09-22 13:52:32 -0300

    Fechando recursos com o banco

--- src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -36,7 +36,8 @@ public class ContaDAO {
             preparedStatement.setString(5,dadosDaConta.dadosCliente().email());
 
             preparedStatement.execute();
-
+            preparedStatement.close();
+            conn.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -44,32 +45,34 @@ public class ContaDAO {
 
     public Set<Conta> listar(){
         Set<Conta> contas = new HashSet<>();
+        PreparedStatement ps;
+        ResultSet rs;
 
         String sql = "SELECT * FROM conta";
 
         try{
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ResultSet resultSet = ps.executeQuery();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
 
-            while(resultSet.next()){
-                Integer numero = resultSet.getInt(1);
-                BigDecimal saldo = resultSet.getBigDecimal(2);
-                String nome = resultSet.getString(3);
-                String cpf = resultSet.getString(4);
-                String email = resultSet.getString(5);
+            while(rs.next()){
+                Integer numero = rs.getInt(1);
+                BigDecimal saldo = rs.getBigDecimal(2);
+                String nome = rs.getString(3);
+                String cpf = rs.getString(4);
+                String email = rs.getString(5);
                 DadosCadastroCliente dadosCadastroCliente = new DadosCadastroCliente(nome, cpf, email);
                 Cliente cliente = new Cliente(dadosCadastroCliente);
 
                 contas.add(new Conta(numero, cliente));
-
             }
 
-        } catch (SQLException e){
+            rs.close();
+            ps.close();
+            conn.close();
+
+        }catch (SQLException e){
             throw new RuntimeException(e);
         }
-
         return contas;
     }
-
-
 }
